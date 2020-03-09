8a07fb76d4d57f5fd7472c33ae057454eb81d7e4
JASON ROUNDTREE <jasontree123@gmail.com>
2020-03-08 19:39:45 -0700

    Replace jquery DOM manipulation with vanilla. Add routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,88 @@
 window.addEventListener('load', () => {
-    const el = $('#app')
+    const el = document.getElementById('app')
+    
+    const errorTemplate = Handlebars.compile(
+        document.getElementById('error-template').innerHTML
+    )
+    const ratesTemplate = Handlebars.compile(
+        document.getElementById('rates-template').innerHTML
+    )
+    const exchangeTemplate = Handlebars.compile(
+        document.getElementById('exchange-template').innerHTML
+    )
+    const historicalTemplate = Handlebars.compile(
+        document.getElementById('historical-template').innerHTML
+    )
 
-    const errorTemplate = Handlebars.compile($('#error-template').html())
-    const ratesTemplate = Handlebars.compile($('#rates-template').html())
-    const exchangeTemplate = Handlebars.compile($('#exchange-template').html())
-    const historicalTemplate = Handlebars.compile($('#historical-template').html())
+    // this was pre-router rendering code:
+    // const html = ratesTemplate()
+    // el.html(html)
 
-    const html = ratesTemplate()
-    el.html(html)
+    const router = new Router({
+        mode: 'history',
+        page404: (path) => {
+            const html = errorTemplate({
+                color: 'yelow',
+                title: 'Error 404 - Page NOT Found',
+                message: `The path '/${path}' does not exist on this site`
+            })
+            el.innerHTML = html
+        }
+    })
+    
+    router.add('/', () => {
+        let html = ratesTemplate()
+        el.innerHTML = html
+    })
+    
+    router.add('/exchange', () => {
+        let html = exchangeTemplate()
+        el.innerHTML = html
+    })
+    
+    router.add('/historical', () => {
+        let html = historicalTemplate()
+        el.innerHTML = html
+    })
+    
+    router.navigateTo(window.location.pathname)
+    
+    const link = $(`a[href$='${window.location.pathname}]`)
+    link.addClass('active')
+    
+    document.addEventListener('click', e => {
+        e.preventDefault()
+        
+        const target = e.target
+        const items = document.querySelectorAll('.item')
+        for (let item of items) {
+            item.classList.remove('active')
+        }
+        target.classList.add('active') 
+
+        const href = target.getAttribute('href')
+        const path = href.substring(href.lastIndexOf('/'))
+        router.navigateTo(path)
+    })
+
+    // with jquery:
+
+    // const errorTemplate = Handlebars.compile($('#error-template').html())
+    // const ratesTemplate = Handlebars.compile($('#rates-template').html())
+    // const exchangeTemplate = Handlebars.compile($('#exchange-template').html())
+    // const historicalTemplate = Handlebars.compile($('#historical-template').html())
+
+    // $('a').on('click', e => {
+    //     console.log('click ran')
+    //     e.preventDefault()
+    
+    //     const target = $(e.target)
+    //     $('.item').removeClass('active')
+    //     target.addClass('active')
+    
+    //     const href = target.attr('href')
+    //     const path = href.substr(href.lastIndexOf('/'))
+    //     router.navigateTo(path)
+    // })
 })
+
--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.use(express.static('public'))
 // allow front-end access to node modules
 app.use('/scripts', express.static(`${__dirname}/node_modules/`))
 
-app.listen(port, () => console.log(`listening on %d`, port))
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`))
+
+app.listen(port, () => console.log(`listening on port %d`, port))
